4c410ce496890a74a9f7d7ef2dd29edebbc79cd1
noa <noa.charbogne@gmail.com>
2025-03-05 16:03:27 +0100

    ex04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -10,15 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fstream>
 #include <iostream>
 #include <string>
+
+void	replaceFile(std::ifstream &in, char **argv)
+{
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	std::string	line;
+	std::string	fileName = std::string(argv[1]) + "_sed.txt";
+	std::ofstream	file(fileName.c_str());
+
+	if (!file){
+		std::cout << "Failed to create " << fileName << std::endl;
+		return ;
+	}	
+	while (std::getline(in, line))
+	{
+		for (size_t i = 0; i <= line.length() ; i++){
+			if ((line.compare(i, s1.length(), s1)) == 0){
+				line.erase(i, s1.length());
+				line.insert(i, s2);
+				i += s2.length();
+			}
+		}
+		file << line << std::endl;
+	}
+	file.close();
+}
 
 int	main(int argc, char **argv)
 {
 	if (argc == 4)
 	{
-		
+		std::ifstream in(argv[1], std::ios_base::in);
+		if (!in.is_open())
+		{
+			std::cout << "File opening failed" << std::endl;
+			return 0;
+		}
+		replaceFile(in, argv);
+		in.close();
 	}
 	else
-		std::cout << "Please comply to : ./sed <filename> <string1> <string2>" :: std<<endl;
+		std::cout << "Please comply to : ./sed <filename> <string1> <string2>" << std::endl;
 }

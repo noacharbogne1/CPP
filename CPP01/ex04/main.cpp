dc21748257f21aa5ad8f140823038d9484bd7fa8
Noa Charbogne <noa.charbogne@orange.fr>
2025-03-05 12:22:30 +0100

    commencer ex04

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 12:16:31 by ncharbog          #+#    #+#             */
+/*   Updated: 2025/03/05 12:22:16 by ncharbog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 4)
+	{
+		
+	}
+	else
+		std::cout << "Please comply to : ./sed <filename> <string1> <string2>" :: std<<endl;
+}

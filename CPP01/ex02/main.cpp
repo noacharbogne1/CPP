8ac9e96e1d36eadda3b93938a1f60b95bac790a3
Noa Charbogne <noa.charbogne@orange.fr>
2025-03-05 10:26:51 +0100

    ex02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 10:17:30 by ncharbog          #+#    #+#             */
+/*   Updated: 2025/03/05 10:26:02 by ncharbog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "Memory address of str: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory adress held by stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}

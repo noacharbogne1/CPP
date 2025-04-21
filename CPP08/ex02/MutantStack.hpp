7832dba714a0a719377291eccbc8fd20ef3a91bc
Noa Charbogne <noa.charbogne@orange.fr>
2025-04-21 13:53:12 +0200

    ex02

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 09:22:43 by ncharbog          #+#    #+#             */
+/*   Updated: 2025/04/21 13:52:42 by ncharbog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+# include <deque>
+
+template<typename T, typename Container = std::stack<T> >
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack() {}
+		MutantStack(const MutantStack &other) {
+			*this = other;
+			return ;
+		}
+		MutantStack &operator=(const MutantStack &other){
+			if (this != &other)
+				std::stack<T>::operator=(other);
+			return *this;
+		}
+		~MutantStack() {}
+
+		typedef typename std::deque<T>::iterator iterator;
+		iterator begin(){
+			return this->c.begin();
+		}
+		iterator end(){
+			return this->c.end();
+		}
+};
+
+#endif
